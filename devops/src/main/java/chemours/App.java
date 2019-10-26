9f1b8823e971075a9d2c5ca4d8a1e04335d254f2
Lokesh Makkineni <lokeshm3108@gmail.com>
2019-10-26 19:18:51 +0530

    updating he code

---
+++ devops/src/main/java/chemours/App.java
@@ -0,0 +1,13 @@
+package chemours;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
